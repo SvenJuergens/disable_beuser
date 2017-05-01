16648f5a8a602dea4cfb986e56fd73d121596d52
SvenJuergens <sj@nordsonne.de>
2017-05-01 16:01:57 +0200

    [TASK] added composer.json // Version 1.3.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,12 +16,12 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'plugin',
     'author' => 'Sven Juergens',
     'author_email' => 'sj@nordsonne.de',
-    'state' => 'beta',
+    'state' => 'stable',
     'internal' => '',
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
-    'version' => '1.3.1',
+    'version' => '1.3.2',
     'constraints' => [
         'depends' => [
             'typo3' => '6.2.0-8.7.99'
@@ -29,6 +29,7 @@ $EM_CONF[$_EXTKEY] = [
         'conflicts' => [
         ],
         'suggests' => [
+            't3_slack' => '1.0.0-1.99.99',
         ],
     ],
 ];
