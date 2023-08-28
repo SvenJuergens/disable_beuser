75e4f559c521aa401626f4608617935a0e449da0
SvenJuergens <SvenJuergens@users.noreply.github.com>
2023-08-28 16:29:14 +0200

    [TASK] replace TYPO3_MODE Constant

--- ext_tables.php
+++ ext_tables.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3_MODE') or die();
+defined('TYPO3') or die();
 
 // Add context sensitive help (csh) for scheduler task
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
