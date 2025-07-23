d089c81651c7c07e86fece33b4b7a05628063508
SvenJuergens <SvenJuergens@users.noreply.github.com>
2025-07-23 22:48:16 +0200

    [TASK] remove deprecated addLLrefForTCAdescr()

--- ext_tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('TYPO3') or die();
-
-// Add context-sensitive help (csh) for scheduler task
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    '_MOD_txdisablebeuser',
-    'EXT:disable_beuser/Resources/Private/Language/locallang_csh_disablebeuser.xlf'
-);
